61f96f79914688cc9c8cf043e7d04914f6c95037
DeviousMalcontent <deviousmalcontent@gmail.com>
2022-10-14 19:17:57 -0800

    Updated tile files and palette

---
+++ tools/MakeTiles.bat
@@ -0,0 +1,22 @@
+CD ..\res\tiles\
+DEL /S *.txt
+CD ..\..\tools\
+
+ImageMagick\Convert ..\res\tiles\tile000.bmp  txt:- >>..\res\tiles\tile000.txt
+ImageMagick\Convert ..\res\tiles\tile001.bmp  txt:- >>..\res\tiles\tile001.txt
+ImageMagick\Convert ..\res\tiles\tile002.bmp  txt:- >>..\res\tiles\tile002.txt
+ImageMagick\Convert ..\res\tiles\tile003.bmp  txt:- >>..\res\tiles\tile003.txt
+ImageMagick\Convert ..\res\tiles\tile004.bmp  txt:- >>..\res\tiles\tile004.txt
+ImageMagick\Convert ..\res\tiles\tile101.bmp  txt:- >>..\res\tiles\tile101.txt
+ImageMagick\Convert ..\res\tiles\tile102.bmp  txt:- >>..\res\tiles\tile102.txt
+ImageMagick\Convert ..\res\tiles\tile103.bmp  txt:- >>..\res\tiles\tile103.txt
+ImageMagick\Convert ..\res\tiles\tile104.bmp  txt:- >>..\res\tiles\tile104.txt
+ImageMagick\Convert ..\res\tiles\tile105.bmp  txt:- >>..\res\tiles\tile105.txt
+ImageMagick\Convert ..\res\tiles\tile106.bmp  txt:- >>..\res\tiles\tile106.txt
+ImageMagick\Convert ..\res\tiles\tile107.bmp  txt:- >>..\res\tiles\tile107.txt
+ImageMagick\Convert ..\res\tiles\tile108.bmp  txt:- >>..\res\tiles\tile108.txt
+ImageMagick\Convert ..\res\tiles\tile109.bmp  txt:- >>..\res\tiles\tile109.txt
+ImageMagick\Convert ..\res\tiles\tile110.bmp  txt:- >>..\res\tiles\tile110.txt
+ImageMagick\Convert ..\res\tiles\tile111.bmp  txt:- >>..\res\tiles\tile111.txt
+ImageMagick\Convert ..\res\tiles\tile112.bmp  txt:- >>..\res\tiles\tile112.txt
+ImageMagick\Convert ..\res\tiles\tile113.bmp  txt:- >>..\res\tiles\tile113.txt
